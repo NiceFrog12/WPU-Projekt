8427a1e3d918b9463216729cf49ace7d349b27bb
NiceFrog12 <156522928+NiceFrog12@users.noreply.github.com>
2025-03-31 10:55:08 +0200

    Update main.py
    added a news and a trashcan picking command

--- main.py
+++ main.py
@@ -19,13 +19,13 @@ welcome_msg = "*Hallo ich bin Nicebot12_*\nIch bin dafür da dein *Leben einfach
 help_msg = "/start\n/help\n/essen\n/alltag\n/fakt\n/rezept"
 
 # Instructions for the Gemini API
-sys_instruct="Du sprichst nur Deutsch und über das Thema Nachhaltigkeit. Alle deine Antworten sollen auf Deutsch sein."
+sys_instruct="Du sprichst nur Deutsch und über das Thema Nachhaltigkeit. Alle deine Antworten sollen auf Deutsch sein. Du kannst simple Form von Markdown nutzen, aber nur in die Form was Telegram Markdown supportet. Als Beispiel, das ist *Bold* und das ist __italicized__"
 client = genai.configure(api_key=GOOGLE_API)
 used_facts = []
 given_tips = []
 food_advice = []
 recipes_used = []
-
+news_used = []
 
 
 @bot.message_handler(commands=['start'])
@@ -45,26 +45,13 @@ def testingcommand(message):
     bot.send_message(message.chat.id, "this is *bold* or *italicized.* another try of __bold__ or _italicized_",parse_mode="Markdown")
 # ´JUST USE MARKDOWN: IT IS NOT WORTH FIGURING OUT MDV2
 
-#       Markdown Telebot Cheatsheet
-#   *bold*
-#   _italicized_
-#   `code`
-#   ```
-#   code multiple liners
-#   ```
-#   ```python
-#   python code multiple liners. Could be bash or shell too.
-#   ```
-
-
-
 @bot.message_handler(commands=["fakt"])
 def send_random_fact(message):
     global used_facts
     try:
         response = model.generate_content(sys_instruct + '\n' + f"Schreib mir ein zufälliger Fakt über Nachhaltigkeit. IF THE FACT IS ALREADY IN {used_facts} YOU CANNOT USE IT AGAIN!!")
         used_facts += [response.text]
-        #response.text is the correct thing you need to pull out
+    #response.text is the correct thing you need to pull out
 
         bot.send_message(message.chat.id, response.text, parse_mode="Markdown")
     except Exception as e:
@@ -109,12 +96,39 @@ def recipe_advice_command(message):
         bot.send_message(message.chat.id, "There has been an error while working through your request.")
         print(e)
 
+@bot.message_handler(commands=["news"])
+def nachhaltigkeit_news(message):
+    global news_used
+    try:
+        response = model.generate_content(sys_instruct + "\n" + f"Schreib mir ein Neuigkeit der was mit Nachhaltigkeit zu tun hat aus moderne Zeit. Es muss irgendwas relevantes sein, die Sprache soll aber simple bleiben. IF THE NEWS ARE ALREADY IN {news_used} YOU CANNOT USE IT AGAIN!!")
+        news_used = [response]
+
+        bot.send_chat_action(message.chat.id, 'typing')
+        time.sleep(3)
+        bot.send_message(message.chat.id, response.text, parse_mode="Markdown")
+    except Exception as e:
+        bot.send_message(message.chat.id, "There has been an error while working through your request.")
+        print(e)
+
+@bot.message_handler(commands=["item"])
+def pick_trashcan(message):
+    try:
+        response = model.generate_content(sys_instruct + "\n" + f"In welche Mülltone soll diese Sache rein gehen: " + message.text)
+
+        bot.send_chat_action(message.chat.id, 'typing')
+        time.sleep(1)
+        bot.send_message(message.chat.id, response.text, parse_mode="Markdown")
+    except Exception as e:
+        bot.send_message(message.chat.id, "There has been an error while working through your request.")
+        print(e)
 
 
 @bot.message_handler(commands=["credits"])
 def credits_command(message):
     credits_message = "Dieser Bot war als ein WPU Projekt erstellt von Aleksandrs Litvins und Fynn Beneke"
     bot.send_message(message.chat.id, credits_message)
+
+
 
 if __name__ == "__main__":
     bot.polling(none_stop=True)
